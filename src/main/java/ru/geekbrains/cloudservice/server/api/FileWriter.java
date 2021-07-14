a2b413699d0107d7916a01b46887dbf346f05885
duckpool <kulgachov88@gmail.com>
2021-07-14 23:17:20 +0300

    Refactor

--- src/main/java/ru/geekbrains/cloudservice/server/api/FileWriter.java
+++ src/main/java/ru/geekbrains/cloudservice/server/api/FileWriter.java
@@ -3,7 +3,6 @@ package ru.geekbrains.cloudservice.server.api;
 import ru.geekbrains.cloudservice.constants.Constants;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.file.Paths;
@@ -21,15 +20,6 @@ public class FileWriter {
         while (position < bytes) {
             position += this.fileChannel.transferFrom(socketChannel, position, Constants.MAX_TRANSFER_VALUE);
         }
-    }
-
-    public int write(ByteBuffer buffer, long position) throws IOException {
-        int byteWritten = 0;
-        while(buffer.hasRemaining()) {
-            byteWritten += this.fileChannel.write(buffer, position + byteWritten);
-        }
-
-        return byteWritten;
     }
 
     public void close() throws IOException {

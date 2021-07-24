6776b4c0a5a2de671c6d9fd7c832df912e5b4193
duckpool <kulgachov88@gmail.com>
2021-07-24 18:29:24 +0300

    Add upload and Download Buttons

---
+++ spring-client/src/test/java/ru/geekbrains/cloudservice/CloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.cloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
